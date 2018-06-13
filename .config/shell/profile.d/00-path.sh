11b7aba5f462b80fc18f0e1863a1eefd69722e51
James Simpson <james.simpson9339@gmail.com>
2018-06-13 11:01:27 -0400

    add prepend action to path creation

--- .config/shell/profile.d/00-path.sh
+++ .config/shell/profile.d/00-path.sh
@@ -2,8 +2,17 @@
 
 function main()
 {
+    function prepend_path()
+    {
+        case ":PATH:" in
+            *":$1:") return ;; # already in path
+            *) [[ -z $PATH ]] && PATH="$1" || PATH="$1:$PATH" ;;
+        esac
+    }
+
     function append_path()
     {
+        echo "working $1"
         case ":PATH:" in
             *":$1:") return ;; # already in path
             *) [[ -z $PATH ]] && PATH="$1" || PATH="$PATH:$1" ;;
@@ -12,7 +21,7 @@ function main()
 
     function check_path()
     {
-        [[ -d "$1" ]] && append_path "$1"
+        [[ -d "$2" ]] && $1 "$2"
     }
 
     # storing default system path and clearing the $PATH variable
@@ -21,24 +30,32 @@ function main()
 
     # adding local bin folder
     [[ -d "$LOCAL_BIN" ]] && {
-        for dir in $LOCAL_BIN/* $LOCAL_BIN ; do check_path $dir ; done
+        for dir in $LOCAL_BIN/* $LOCAL_BIN ; do check_path append_path $dir ; done
     }
     
     # adding local lib folder
     [[ -d "$LOCAL_LIB" ]] && {
-        for dir in $LOCAL_LIB/* $LOCAL_LIB ; do check_path $dir ; done
+        for dir in $LOCAL_LIB/* $LOCAL_LIB ; do check_path append_path $dir ; done
     }
 
-    append_path $system_path
+    append_path /usr/local/bin
+    append_path /usr/local/sbin
+    append_path /usr/local/opt/srm/bin
+
+    # Adding system path back in
+    local sys_path_array=
+    IFS=':' read -ra sys_path_array <<< "$system_path"
+    for i in "${sys_path_array[@]}" ; do append_path $i ; done
 
     # On windows I have some programs that i use a lot on the command line. I want the windows
     # version of these programs instead of the version that is installed by something like
     # pacman on msys or something like that. These are the default folder locations.
     # If these directories exist then append them to the $PATH variable so I can use them
-    check_path "/c/Program Files/CMake/bin"
-    check_path "/c/Program Files/Microsoft VS Code/bin"
+    check_path prepend_path "/c/Program Files/CMake/bin"
+    check_path append_path  "/c/Program Files/Microsoft VS Code/bin"
 
     export PATH
+    unset prepend_path
     unset append_path
     unset check_path
 }
