1aeb1bbf447764290b7e4bf9f84f0837c6dbda08
James Simpson <james.simpson9339@gmail.com>
2018-12-07 16:24:51 -0500

    add vscode extensions install

---
+++ .config/code/install-extensions.sh
@@ -0,0 +1,6 @@
+code --install-extension IronLu233.vscode-color-exchange
+code --install-extension kamikillerto.vscode-colorize
+code --install-extension kuscamara.electron
+code --install-extension pranaygp.vscode-css-peek
+code --install-extension stuart.unique-window-colors
+code --install-extension Youssef.viow
