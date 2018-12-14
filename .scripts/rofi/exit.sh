266ccfc60008ecf71bbb73a0d188c4a39c8235ab
James Simpson <james.simpson9339@gmail.com>
2018-12-14 16:55:44 -0500

    lockscreen script

--- .scripts/rofi/exit.sh
+++ .scripts/rofi/exit.sh
@@ -3,7 +3,7 @@
 dialog=$(echo -e 'lock\nsuspend\nhibernate\nshutdown\nreboot\nexit i3' | rofi -dmenu -i -p "[Exit option]" -hide-scrollbar -tokenize -lines 6 -eh 2 -width 40 -location 8 -xoffset 150 -yoffset 70 -padding 50 -font "inconsolata 16")
 
 case $dialog in
-    lock)      i3lock ;;
+    lock)      $HOME/.scripts/lockscreen.sh ;;
     suspend)   systemctl suspend ;;
     hibernate) systemctl hibernate ;;
     shutdown)  systemctl poweroff ;;
