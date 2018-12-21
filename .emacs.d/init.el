6c1e07930e0948588d944f7a24ba77842506a89c
James Simpson <james.simpson9339@gmail.com>
2018-12-21 11:32:18 -0500

    update init for emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,14 @@
+(require 'package)
+
+;; Adding package repositories to the list of avalible packages
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+;; Remove all gui elements from ui
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
