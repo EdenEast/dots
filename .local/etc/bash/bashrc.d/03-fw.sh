587f3ca7985b66753a725c696578b0826a3c47db
James Simpson <james.simpson9339@gmail.com>
2019-03-11 20:09:18 -0400

    update fw setup for bash

--- .local/etc/bash/bashrc.d/03-fw.sh
+++ .local/etc/bash/bashrc.d/03-fw.sh
@@ -1,7 +1,1 @@
-if [[ -x $(command -v fw) ]]; then
-    if [[ -x "$(command -v fzf)" ]]; then
-        eval "$(fw print-bash-setup -f 2>/dev/null)"
-    else
-        eval "$(fw print-bash-setup 2>/dev/null)"
-    fi
-fi
+[[ -x $(command -v fw) ]] && eval "$(fw print-bash-setup 2>/dev/null)"
