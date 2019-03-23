9dffb1aae5aa7c4332ef45df7c15a0d520b67826
James Simpson <james.simpson9339@gmail.com>
2019-03-23 12:46:23 -0400

    restart awesome config

--- .config/awesome/core/init.lua
+++
@@ -1,9 +0,0 @@
-local setmetatable = setmetatable
-
-return setmetatable(
-	{ _NAME = "core" },
-	{ __index = function(table, key)
-		local module = rawget(table, key)
-		return module or require(table._NAME .. '.' .. key)
-	end }
-)
