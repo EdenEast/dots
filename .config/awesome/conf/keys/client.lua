5c203e6758739ec20c05fe2a131499a253d18277
James Simpson <james.simpson9339@gmail.com>
2019-03-25 19:15:00 -0400

    update client keys

--- .config/awesome/conf/keys/client.lua
+++ .config/awesome/conf/keys/client.lua
@@ -7,47 +7,46 @@ local ctrl = 'Control'
 local shift = 'Shift'
 
 local clientkeys = gears.table.join(
-       awful.key({ mod,           }, "f",
-        function (c)
+    awful.key({ mod }, 'f',
+        function(c)
             c.fullscreen = not c.fullscreen
             c:raise()
         end,
-        {description = "toggle fullscreen", group = "client"}),
-    awful.key({ mod, }, "BackSpace",      function (c) c:kill()                         end,
-              {description = "close", group = "client"}),
-    awful.key({ mod, ctrl }, "space",  awful.client.floating.toggle                     ,
-              {description = "toggle floating", group = "client"}),
-    awful.key({ mod, ctrl }, "Return", function (c) c:swap(awful.client.getmaster()) end,
-              {description = "move to master", group = "client"}),
-    awful.key({ mod,           }, "o",      function (c) c:move_to_screen()               end,
-              {description = "move to screen", group = "client"}),
-    awful.key({ mod,           }, "t",      function (c) c.ontop = not c.ontop            end,
-              {description = "toggle keep on top", group = "client"}),
-    awful.key({ mod,           }, "n",
-        function (c)
-            -- The client currently has the input focus, so it cannot be
-            -- minimized, since minimized clients can't have the focus.
-            c.minimized = true
-        end ,
-        {description = "minimize", group = "client"}),
-    awful.key({ mod,           }, "m",
-        function (c)
+        { description = 'Toggle fullscreen', group = 'Client' }
+    ),
+    awful.key({ mod, ctrl }, 'f',
+        awful.client.floating.toggle,
+        -- function(c)
+        --     c.fullscreen = not c.fullscreen
+        --     c:raise()
+        -- end,
+        { description = 'Toggle fullscreen', group = 'Client' }
+    ),
+    awful.key({ mod }, 'BackSpace',
+        function(c)
+            c:kill()
+        end,
+        { description = 'Close', group = 'Client' }
+    ),
+    awful.key({ mod }, 't',
+        function(c)
+            c.ontop = not c.ontop
+        end,
+        { description = 'Toggle keep on top', group = 'Client' }
+    ),
+    awful.key({ mod }, 'n',
+        function(c)
+            c.minimized = not c.minimized
+        end,
+        { description = 'Minimize', group = 'Client' }
+    ),
+    awful.key({ mod }, 'm',
+        function(c)
             c.maximized = not c.maximized
             c:raise()
-        end ,
-        {description = "(un)maximize", group = "client"}),
-    awful.key({ mod, ctrl }, "m",
-        function (c)
-            c.maximized_vertical = not c.maximized_vertical
-            c:raise()
-        end ,
-        {description = "(un)maximize vertically", group = "client"}),
-    awful.key({ mod, shift   }, "m",
-        function (c)
-            c.maximized_horizontal = not c.maximized_horizontal
-            c:raise()
-        end ,
-        {description = "(un)maximize horizontally", group = "client"})
+        end,
+        { description = 'Maximize', group = 'Client' }
+    )
 )
 
 return clientkeys
