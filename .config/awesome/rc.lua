0bf51913eedeceaf84a9cb881ab4e01094229589
James Simpson <james.simpson9339@gmail.com>
2019-03-25 23:55:07 -0400

    move error handling into it's own module

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -1,51 +1,8 @@
--- If LuaRocks is installed, make sure that packages installed through it are
--- found (e.g. lgi). If LuaRocks is not installed, do nothing.
-pcall(require, "luarocks.loader")
-
--- Standard awesome library
-local gears = require("gears")
 local awful = require("awful")
-require("awful.autofocus")
--- Widget and layout library
-local wibox = require("wibox")
--- Theme handling library
 local beautiful = require("beautiful")
--- Notification library
-local naughty = require("naughty")
-local menubar = require("menubar")
-local hotkeys_popup = require("awful.hotkeys_popup")
--- Enable hotkeys help widget for VIM and other apps
--- when client with a matching name is opened:
-require("awful.hotkeys_popup.keys")
-
--- {{{ Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
-if awesome.startup_errors then
-    naughty.notify({ preset = naughty.config.presets.critical,
-                     title = "Oops, there were errors during startup!",
-                     text = awesome.startup_errors })
-end
-
--- Handle runtime errors after startup
-do
-    local in_error = false
-    awesome.connect_signal("debug::error", function (err)
-        -- Make sure we don't go into an endless error loop
-        if in_error then return end
-        in_error = true
-
-        naughty.notify({ preset = naughty.config.presets.critical,
-                         title = "Oops, an error happened!",
-                         text = tostring(err) })
-        in_error = false
-    end)
-end
--- }}}
-
----------------------------------------------------------------------------------------------------
 
 -- Init all modules
+require('module.error-check')
 require('module.decorate-client')
 require('module.exit-screen')
 require('conf.tags')
@@ -85,6 +42,11 @@ client.connect_signal("unfocus",
 )
 
 ---------------------------------------------------------------------------------------------------
+
+local gears = require("gears")
+local wibox = require("wibox")
+local naughty = require("naughty")
+local menubar = require("menubar")
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
