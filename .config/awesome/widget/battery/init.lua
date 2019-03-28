8935028834d75a9df9bd1bc3fd2b373cab1c50bf
James Simpson <james.simpson9339@gmail.com>
2019-03-27 23:40:17 -0400

    add battery precentage

---
+++ .config/awesome/widget/battery/init.lua
@@ -0,0 +1,35 @@
+local awful = require("awful")
+local gears = require("gears")
+local wibox = require("wibox")
+local beautiful = require("beautiful")
+
+-- Configuration
+local update_interval = 30            -- in seconds
+
+local battery = wibox.widget{
+    text = "battery widget",
+    align  = 'center',
+    valign = 'center',
+    widget = wibox.widget.textbox
+}
+
+local function update_widget(bat)
+  battery.markup = bat .. "%"
+end
+
+local bat_script = [[
+  bash -c '
+  upower -i $(upower -e | grep BAT) | grep percentage
+  ']]
+
+awful.widget.watch(
+    bat_script, update_interval,
+    function(widget, stdout)
+        local bat = stdout:match(':%s*(.*)..')
+        -- bat = string.gsub(bat, '^%s*(.-)%s*$', '%1')
+        update_widget(bat)
+    end
+)
+
+return battery
+
