619a65853f3c0ff359fa1e45778b14e1f3d86b3f
James Simpson <james.simpson9339@gmail.com>
2019-07-09 11:16:03 -0400

    change vscode extension update alias to a function that generates a script

--- .config/code/install-extensions.sh
+++ .config/code/install-extensions.sh
@@ -1,69 +1,129 @@
-code --install-extension ahmadawais.shades-of-purple
-code --install-extension alefragnani.Bookmarks
-code --install-extension alefragnani.project-manager
-code --install-extension Alexey-Strakh.stackoverflow-search
-code --install-extension annliu.1987
-code --install-extension archipa.icl
-code --install-extension belfz.search-crates-io
-code --install-extension brittanychiang.halcyon-vscode
-code --install-extension bungcip.better-toml
-code --install-extension cheshirekow.cmake-format
-code --install-extension christian-kohler.path-intellisense
-code --install-extension codezombiech.gitignore
-code --install-extension Compulim.vscode-clock
-code --install-extension DavidAnson.vscode-markdownlint
-code --install-extension dericcain.feather
-code --install-extension dullptr.envytheme
-code --install-extension eamodio.gitlens
-code --install-extension EditorConfig.EditorConfig
-code --install-extension endorfina.roseate
-code --install-extension eppz.eppz-code
-code --install-extension ExodiusStudios.comment-anchors
-code --install-extension fabiospampinato.vscode-todo-plus
-code --install-extension fallenwood.vimL
-code --install-extension file-icons.file-icons
-code --install-extension formulahendry.auto-rename-tag
-code --install-extension ginfuru.ginfuru-onedark-raincoat-theme
-code --install-extension huizhou.githd
-code --install-extension janjoerke.align-by-regex
-code --install-extension jolaleye.horizon-theme-vscode
-code --install-extension josa.markdown-table-formatter
-code --install-extension josef.rouge-theme
-code --install-extension joshpeng.sublime-babel-vscode
-code --install-extension joytrekker.rainier
-code --install-extension JScearcy.rust-doc-viewer
-code --install-extension juanmnl.vscode-theme-1984
-code --install-extension kamikillerto.vscode-colorize
-code --install-extension kevinkyang.auto-comment-blocks
-code --install-extension lunarlimbo.banner-comments-plus
-code --install-extension luqimin.tiny-light
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension ms-vscode.cpptools
-code --install-extension ms-vscode.csharp
-code --install-extension ms-vscode.powershell
-code --install-extension ms-vscode.vscode-typescript-tslint-plugin
-code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension Nimda.deepdark-material
-code --install-extension NoParanoia.noparanoia
-code --install-extension olveralan.spunbeat
-code --install-extension philsinatra.nested-comments
-code --install-extension piotrpalarz.vscode-gitignore-generator
-code --install-extension pnp.polacode
-code --install-extension rafamel.subtle-brackets
-code --install-extension RobbOwen.synthwave-vscode
-code --install-extension rpinski.shebang-snippets
-code --install-extension rust-lang.rust
-code --install-extension sanderledegen.angular-follow-selector
-code --install-extension SanderPilon.blockalign
-code --install-extension serayuzgur.crates
-code --install-extension skellock.just
-code --install-extension smlombardi.slime
-code --install-extension stackbreak.comment-divider
-code --install-extension stkb.rewrap
-code --install-extension tinkertrain.theme-panda
-code --install-extension tourervit.phantom
-code --install-extension twxs.cmake
-code --install-extension vincaslt.highlight-matching-tag
-code --install-extension VisualStudioExptTeam.vscodeintellicode
-code --install-extension wmaurer.change-case
-code --install-extension yo1dog.cursor-align
+#!/usr/bin/env bash
+
+extension_list=(
+ahmadawais.shades-of-purple
+alefragnani.Bookmarks
+alefragnani.project-manager
+Alexey-Strakh.stackoverflow-search
+annliu.1987
+archipa.icl
+belfz.search-crates-io
+brittanychiang.halcyon-vscode
+bungcip.better-toml
+cheshirekow.cmake-format
+christian-kohler.path-intellisense
+codezombiech.gitignore
+Compulim.vscode-clock
+DavidAnson.vscode-markdownlint
+dericcain.feather
+dullptr.envytheme
+eamodio.gitlens
+EditorConfig.EditorConfig
+endorfina.roseate
+eppz.eppz-code
+ExodiusStudios.comment-anchors
+fabiospampinato.vscode-todo-plus
+fallenwood.vimL
+file-icons.file-icons
+formulahendry.auto-rename-tag
+ginfuru.ginfuru-onedark-raincoat-theme
+huizhou.githd
+janjoerke.align-by-regex
+jolaleye.horizon-theme-vscode
+josa.markdown-table-formatter
+josef.rouge-theme
+joshpeng.sublime-babel-vscode
+joytrekker.rainier
+JScearcy.rust-doc-viewer
+juanmnl.vscode-theme-1984
+kamikillerto.vscode-colorize
+kevinkyang.auto-comment-blocks
+lunarlimbo.banner-comments-plus
+luqimin.tiny-light
+ms-azuretools.vscode-docker
+ms-vscode.cpptools
+ms-vscode.csharp
+ms-vscode.powershell
+ms-vscode.vscode-typescript-tslint-plugin
+msjsdiag.debugger-for-chrome
+Nimda.deepdark-material
+NoParanoia.noparanoia
+olveralan.spunbeat
+philsinatra.nested-comments
+piotrpalarz.vscode-gitignore-generator
+pnp.polacode
+rafamel.subtle-brackets
+RobbOwen.synthwave-vscode
+rpinski.shebang-snippets
+rust-lang.rust
+sanderledegen.angular-follow-selector
+SanderPilon.blockalign
+serayuzgur.crates
+skellock.just
+smlombardi.slime
+stackbreak.comment-divider
+stkb.rewrap
+tinkertrain.theme-panda
+tourervit.phantom
+twxs.cmake
+vincaslt.highlight-matching-tag
+VisualStudioExptTeam.vscodeintellicode
+wmaurer.change-case
+yo1dog.cursor-align
+)
+
+function main()
+{
+    local current_ext_list=($(code --list-extensions))
+    local skip_count=0
+    local install_count=0
+
+    local a=()
+    local name=''
+
+    for ext in ${extension_list[@]}; do
+        a=(${ext//./ })
+        name=${a[1]}
+
+        if [[ ! "${current_ext_list[@]}" =~ "$ext" ]]; then
+            echo "Installing: $name..."
+            install "$ext"
+            install_count=$((install_count+1))
+        else
+            echo "Skipping:   $name..."
+            skip_count=$((skip_count+1))
+        fi
+    done
+
+    # determine if there are any local extensions that are on in the saved list
+    local local_extensions=()
+    local local_count=0
+    for ext in ${current_ext_list}; do
+        a=(${ext//./ })
+        name=${a[1]}
+
+        if [[ ! "$extension_list[@]" =~ "$ext" ]]; then
+            local_extensions+=($name)
+            local_count=$((local_count+1))
+        fi
+    done
+
+    echo ""
+    echo "Complete! Results:"
+    echo "  Installed: $install_count"
+    echo "  Skipped:   $skip_count"
+    if [[ $local_count -gt 0 ]]; then
+        echo ""
+        echo "There are currently $local_count extensions installed that are not saved:"
+        for e in ${local_extensions[@]}; do
+            echo "  $e"
+        done
+    fi
+}
+
+function install()
+{
+    code --install-extension "$1" 1>/dev/null
+}
+
+main "$@"
