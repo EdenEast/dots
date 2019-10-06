2b073260974f5293aa6dc7261b38cc63702f87ce
James Simpson <james.simpson9339@gmail.com>
2019-10-06 00:51:12 -0400

    remove quit option for config edit script

--- .script/rofi/edit-config.sh
+++ .script/rofi/edit-config.sh
@@ -22,21 +22,20 @@ xmonad
 xrdb
 xresource
 zsh
-quit
 ")
 
 choice=$(echo -e "${options[@]}" | rofi -dmenu -i -p 'Edit config file' -hide-scrollbar --tokenize)
 
 case $choice in
-    awesome   ) open_dir     $HOME /.config/awesome   ;;
-    bash      ) open_file    $HOME /.bashrc           ;;
-    git       ) open_dir     $HOME /.config/git       ;;
-    i3        ) open_file    $HOME /.config/i3/config ;;
-    polybar   ) open_file    $HOME /.config/polybar   ;;
-    qtile     ) open_file    $HOME /.config/qtile     ;;
-    xmonad    ) open_dir     $HOME /.xmonad           ;;
-    vim       ) open_dir     $HOME /.config/vim       ;;
-    xrdb      ) open_dir     $HOME /.config/xrdb      ;;
-    xresource ) open_file    $HOME /.XResources       ;;
-    zsh       ) open_dir vim $HOME /.config/zsh       ;;
+    awesome   ) open_dir     $HOME/.config/awesome   ;;
+    bash      ) open_file    $HOME/.bashrc           ;;
+    git       ) open_dir     $HOME/.config/git       ;;
+    i3        ) open_file    $HOME/.config/i3/config ;;
+    polybar   ) open_file    $HOME/.config/polybar   ;;
+    qtile     ) open_file    $HOME/.config/qtile     ;;
+    xmonad    ) open_dir     $HOME/.xmonad           ;;
+    vim       ) open_dir     $HOME/.config/vim       ;;
+    xrdb      ) open_dir     $HOME/.config/xrdb      ;;
+    xresource ) open_file    $HOME/.XResources       ;;
+    zsh       ) open_dir vim $HOME/.config/zsh       ;;
 esac
