f002ce9a9ed9df774a2652ecd0b261b1de6ddc52
James Simpson <james.simpson9339@gmail.com>
2019-10-06 21:00:34 -0400

    repo config path defines

--- .config/shell/profile.d/01-config-paths.sh
+++ .config/shell/profile.d/01-config-paths.sh
@@ -1,2 +1,7 @@
+
+# repo config path defineds
+export REPO_CONFIG_PATH="$XDG_CONFIG_HOME/repo"
+export REPO_LOCAL_PATH="$LOCAL_ETC/repo"
+
 export VIMINIT='let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | source $MYVIMRC'
 export VIMDOTDIR="$XDG_CONFIG_HOME/vim"
