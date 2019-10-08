af445de8fa7dee2924e3d15b22a63e042c38daf9
James Simpson <james.simpson9339@gmail.com>
2019-10-08 17:48:50 -0400

    add YADM_DIR export to set dir

--- .config/shell/profile.d/01-config-paths.sh
+++ .config/shell/profile.d/01-config-paths.sh
@@ -5,3 +5,5 @@ export REPO_LOCAL_PATH="$LOCAL_ETC/repo"
 
 export VIMINIT='let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | source $MYVIMRC'
 export VIMDOTDIR="$XDG_CONFIG_HOME/vim"
+
+export YADM_DIR="$XDG_CONFIG_HOME/yadm"
