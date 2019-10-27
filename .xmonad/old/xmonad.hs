24b98ecbfad38b6e67d05027f9d188c862bf9d80
James Simpson <james.simpson9339@gmail.com>
2019-10-27 00:18:42 -0400

    [xmonad] move old config and start with dt's

---
+++ .xmonad/old/xmonad.hs
@@ -0,0 +1,67 @@
+import Control.Monad
+import Data.Monoid
+import XMonad
+import XMonad.Actions.DynamicProjects
+import XMonad.Actions.Navigation2D
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.NamedActions
+import XMonad.Util.Replace
+import XMonad.Util.Run
+
+-- ./lib/
+import Bind.Master
+import Bus.Events
+import Bus.Hooks
+import Config.Options
+import Config.Projects
+import Container.Layout
+import Container.Navigation
+
+-- Defaults
+-------------------------------------------------------------------------------
+defaults = def {
+  -- simple stuff
+  terminal           = term options,
+  focusFollowsMouse  = ffm options,
+  modMask            = mask options,
+  workspaces         = spaces options,
+
+  normalBorderColor  = unfocused theme,
+  focusedBorderColor = focused theme,
+  borderWidth        = border theme,
+
+  -- key bindings
+  -- keys               = keyBindings,
+  mouseBindings      = mouseBindings',
+
+  layoutHook         = layout,
+  manageHook         = hooks,
+  handleEventHook    = events options,
+  logHook            = logHook',
+  startupHook        = starts options
+}
+
+main :: IO ()
+main = do
+  -- xmproc0 <- spawnPipe "xmobar -x 0 $HOME/.config/xmobar/xmobarrc"
+  -- xmproc1 <- spawnPipe "xmobar -x 1 $HOME/.config/xmobar/xmobarrc"
+  -- xmproc2 <- spawnPipe "xmobar -x 2 $HOME/.config/xmobar/xmobarrc"
+  replace
+
+  -- forM_ [ "xmonad-ws"
+  --       , "xmonad-layout"
+  forM_ [ ".xmonad-title-log"
+        , ".xmonad-workspace-log"
+        -- , "caffeine"
+        ]
+        $ \file -> safeSpawn "mkfifo" ["/tmp/"++file]
+
+  xmonad
+    . docks
+    . addDescrKeys' ((mask options, xK_F1), showKeybindings) keyBindings
+    . ewmh
+    . navigate
+    . dynamicProjects projects
+    . withNavigation2DConfig nav2dConfig
+    $ defaults
--- .xmonad/readme.md
+++ .xmonad/readme.md
@@ -1,12 +1,4 @@
-# xmonad
 
-## Resources
 
-- [XmoNORD](https://www.reddit.com/r/unixporn/comments/cmenre/xmonad_xmonord_update/)
-- [thomasf](https://github.com/thomasf/dotfiles-thomasf-xmonad)
-    - good way of handling key bindings
-- [ethan schoonover](https://github.com/altercation/dotfiles-tilingwm)
-    - good layout design and interesting bindings. handles layouts well
-    - xmonad [demo](https://youtu.be/70IxjLEmomg)
-- [spacemacs in xmonad?](https://blog.pclewis.com/2016/03/19/xmonad-spacemacs-style.html)
-- [might be good for handling keys](https://github.com/maxigit/xmonad-config/blob/master/xmonad.hs)
+[WhiteAbeLincoln](https://github.com/WhiteAbeLincoln/abes-xmonad/blob/master/src/xmonad.hs) interesting way to handle
+key bindings. Also clean config file and easy to understand
