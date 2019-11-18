4cb1501c284f8b07d9605328b5cf192afdc17a89
James Simpson <james.simpson9339@gmail.com>
2019-11-18 16:59:01 -0500

    [sccache] set SCCACHE_DIR to always be cache dir
    new path is always $HOME/.cache/sccache

--- .config/shell.d/rc.d/005-application-setting.sh
+++ .config/shell.d/rc.d/005-application-setting.sh
@@ -2,7 +2,10 @@
 [[ -x "$(command -v repo)" ]] && eval "$(repo script)"
 
 # if sccache is installed add that as a rustc wrapper
-[[ -x "$(command -v sccache)" ]] && export RUSTC_WRAPPER="sccache"
+[[ -x "$(command -v sccache)" ]] && {
+    export RUSTC_WRAPPER="sccache"
+    export SCCACHE_DIR="$HOME/.cache/sccache"
+}
 
 # if jump is installed then source 'jump shell'
 [[ -x "$(command -v jump)" ]] && eval "$(jump shell)"
