7d17345ef7727a3ee26cdc0a3e9bd8bb395caab3
James Simpson <james.simpson9339@gmail.com>
2020-02-11 23:18:38 -0500

    [starship] starship setup and config

--- .config/shell.d/bash.d/010-prompt.bash
+++ .config/shell.d/bash.d/010-prompt.bash
@@ -1,1 +1,3 @@
+export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
+
 [ -n "$(command -v starship)" ] && eval "$(starship init bash)" || source "$HOME/.config/shell.d/bash.d/010-minimal-prompt"
