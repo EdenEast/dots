d85c94b040378fadd98fc6870db54179cceb1f55
James Simpson <james.simpson9339@gmail.com>
2020-02-12 10:34:47 -0500

    [starship] add comment to remove starship if required

--- .config/shell.d/bash.d/010-prompt.bash
+++ .config/shell.d/bash.d/010-prompt.bash
@@ -1,3 +1,5 @@
 export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
 
+# source "$HOME/.config/shell.d/bash.d/010-minimal-prompt"
 [ -n "$(command -v starship)" ] && eval "$(starship init bash)" || source "$HOME/.config/shell.d/bash.d/010-minimal-prompt"
+
