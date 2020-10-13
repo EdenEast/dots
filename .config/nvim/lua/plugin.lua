71ceb241159c4f2186d8d5d6f3ae5610a14fd45c
James Simpson <edenofest@gmail.com>
2020-10-13 17:34:33 -0400

    [nvim] Add modtion and text objects

--- .config/nvim/lua/plugin.lua
+++ .config/nvim/lua/plugin.lua
@@ -38,12 +38,29 @@ return require('packer').startup({
 
     -- Util and quality of life
     use {
-      'airblade/vim-rooter'
+      'airblade/vim-rooter',
+      'markonm/traces.vim',   -- Improved searching
+      'jiangmiao/auto-pairs', -- Insert or delete brackets, parens, quotes in pair.
+    }
+
+    -- Motions and text objects
+    use {
+      'tpope/vim-commentary',        -- Language agnostic comment motions
+      'junegunn/vim-easy-align',
+      'machakann/vim-sandwich',      -- Search/select/edit sandwiched textobjects (surrond)
+      'christoomey/vim-sort-motion',
+      'glts/vim-textobj-comment',    -- Text objects for comments
+      'wellle/targets.vim',          -- Add text objects for pair, quote, seperator, argument, and tag
+      'wellle/line-targets.vim',     -- Add - object for line 'da-'
+      'kana/vim-textobj-user',       -- Create own user text objects
+      'AndrewRadev/sideways.vim',    -- Move the item under the cursor left or right, where an "item" is defined by a delimiter.
     }
 
     -- Themes
-    use 'arzg/vim-colors-xcode'
-    use 'sainnhe/gruvbox-material'
+    use {
+      'arzg/vim-colors-xcode',
+      'sainnhe/gruvbox-material',
+    }
   end,
   config = {
     package_root = string.format("%s/site/pack", vim.g.cache_root),
