3749f83fd5632bb06a46ace64d106d3adb2c6c09
James Simpson <edenofest@gmail.com>
2020-10-26 01:41:39 -0400

    [nvim] Add lf integration

--- .config/nvim/lua/pack/search.lua
+++ .config/nvim/lua/pack/search.lua
@@ -7,3 +7,15 @@ use {
     run = function() vim.fn['fzf#install']() end,
   },
 }
+
+-- File browser
+use {
+  'ptzz/lf.vim',
+  requires = { 'rbgrouleff/bclose.vim' },
+  cmd = { 'Lf', 'LfCurrentFile', 'LfCurrentDirectory', 'LfWorkingDirectory', },
+  config = function()
+    vim.g.lf_map_keys = 0
+    vim.g.lf_replace_netrw = true
+    vim.g.lf_command_override = 'lf -command "map e open"'
+  end,
+}
